9a16eadc56eb11543a434f1e87a09c282c0657ca
Harold Vilander <harold.vilander@gmail.com>
2019-02-28 16:26:52 -0600

    added template config file

---
+++ TEMPLATE_config.py
@@ -0,0 +1,5 @@
+# config.py
+# save a copy of this template file as config.py with your BOT TOKEN from discord app settings 
+# Client secrect needs tobe in quotes
+
+bot_token = "YOUR_CLIENT_SECRET_GOES_HERE"
