bb6ba7736f92e2c1681eb5b9377174d89eda900e
Harold Vilander <harold.vilander@gmail.com>
2019-02-28 16:40:48 -0600

    fixed comment

--- TEMPLATE_config.py
+++ TEMPLATE_config.py
@@ -1,6 +1,6 @@
 # config.py
 # save a copy of this template file as config.py with your BOT TOKEN from discord app settings 
-# Client secrect needs tobe in quotes
+# and your Giphy api key, both are strings and so need to be in quotes
 
 bot_token = "YOUR_CLIENT_SECRET_GOES_HERE"
 giphy_api_key = "YOUR_GIPHY_API_KEY"
