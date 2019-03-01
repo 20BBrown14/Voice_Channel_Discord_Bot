2e39a98b5e628fbe84d325d9819feb1d3d9a35a5
Harold Vilander <hvilander@users.noreply.github.com>
2019-03-01 10:22:36 -0600

    Added a Config template & updated to new version number format

---
+++ TEMPLATE_config.py
@@ -0,0 +1,6 @@
+# config.py
+# save a copy of this template file as config.py with your BOT TOKEN from discord app settings 
+# and your Giphy api key, both are strings and so need to be in quotes
+
+bot_token = "BOT_TOKEN"
+giphy_api_key = "YOUR_GIPHY_API_KEY"
