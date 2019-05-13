7d67fc045530aad714541c712ce3939bc9885f42
20bbrown14 <20bbrown14@gmail.com>
2019-05-13 09:20:28 -0500

    Version bump

--- commands/single_giphy_results_display.py
+++ commands/single_giphy_results_display.py
@@ -14,20 +14,28 @@ async def command(client, message, channel, delete_message):
   single_results_array = []
   single_results_string = ''
   single_results_count = 0
-  if(os.path.isfile('single_giphy_results.txt')):
-    with open('single_giphy_results.txt') as f:
-      for line in f:
-        if(len(single_results_string) + len(line.rstrip() + ', ') < 1950):
-          single_results_string = single_results_string + line.rstrip() + ', '
-          single_results_count += 1
-        else:
-          single_results_array.append(single_results_string)
-          single_results_string = '' + line.rstrip() + ', '
-          single_results_count += 1
-      single_results_array.append(single_results_string)
-    await print_results(client, message, channel, single_results_array, single_results_count)
+  if(message.content == '!singleresults'):
+    if(os.path.isfile('single_giphy_results.txt')):
+      with open('single_giphy_results.txt') as f:
+        for line in f:
+          if(len(single_results_string) + len(line.rstrip() + ', ') < 1950):
+            single_results_string = single_results_string + line.rstrip() + ', '
+            single_results_count += 1
+          else:
+            single_results_array.append(single_results_string)
+            single_results_string = '' + line.rstrip() + ', '
+            single_results_count += 1
+        single_results_array.append(single_results_string)
+      await print_results(client, message, channel, single_results_array, single_results_count)
+    else:
+      await client.send_message(message.author, "There are no single results yet")
   else:
-    await client.send_message(message.author, "There are no single results yet")
-
+    if(os.path.isfile('single_giphy_results.txt')):
+      with open('single_giphy_results.txt') as f:
+        for line in f:
+          single_results_count += 1
+      await client.send_message(channel, "There have been %d single results so far" % single_results_count)
+    else:
+      await client.send_message(message.author, "There are no single results yet")
 
 TRIGGER = '!singleresults'
