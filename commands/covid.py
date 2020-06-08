92dab2a6479d7063d8d4c5a625471ade2b3c929a
20bbrown14 <20bbrown14@gmail.com>
2020-06-08 10:32:31 -0500

    Fix issues with null data

--- commands/covid.py
+++ commands/covid.py
@@ -29,25 +29,25 @@ async def command(client, message):
         return 0
       world_cases = world_active = world_deaths = us_cases = us_active = us_deaths = 0
       if(data[0]['country'] == 'World'):
-        world_cases = '{:,}'.format(data[0]['cases'])
-        world_active = '{:,}'.format(data[0]['active'])
-        world_deaths = '{:,}'.format(data[0]['deaths'])
+        world_cases = '{:,}'.format(data[0]['cases'] if data[0]['cases'] else 0)
+        world_active = '{:,}'.format(data[0]['active'] if data[0]['active'] else 0)
+        world_deaths = '{:,}'.format(data[0]['deaths'] if data[0]['deaths'] else 0)
       else:
         for country in data:
           if(country['country'] == 'World'):
-            world_cases = '{:,}'.format(country['cases'])
-            world_active = '{:,}'.format(country['active'])
-            world_deaths = '{:,}'.format(country['deaths'])
+            world_cases = '{:,}'.format(country['cases'] if country['cases'] else 0)
+            world_active = '{:,}'.format(country['active'] if country['active'] else 0)
+            world_deaths = '{:,}'.format(country['deaths'] if country['deaths'] else 0)
             break
       if(data[1]['country'] == 'USA'):
-        us_cases = '{:,}'.format(data[1]['cases'])
-        us_active = '{:,}'.format(data[1]['active'])
-        us_deaths = '{:,}'.format(data[1]['deaths'])
+        us_cases = '{:,}'.format(data[1]['cases'] if data[1]['cases'] else 0)
+        us_active = '{:,}'.format(data[1]['active'] if data[1]['active'] else 0)
+        us_deaths = '{:,}'.format(data[1]['deaths'] if data[1]['deaths'] else 0)
       else:
         for country in data:
-          us_cases = '{:,}'.format(country['cases'])
-          us_active = '{:,}'.format(country['active'])
-          us_deaths = '{:,}'.format(country['deaths'])
+          us_cases = '{:,}'.format(country['cases'] if country['cases'] else 0)
+          us_active = '{:,}'.format(country['active'] if country['active'] else 0)
+          us_deaths = '{:,}'.format(country['deaths'] if country['deaths'] else 0)
           break
       message_to_send = "World Cases: %s\nWorld Active: %s\nWorld Deaths: %s\nUS Cases: %s\nUS Active: %s\nUS Deaths: %s" % (world_cases, world_active, world_deaths, us_cases, us_active, us_deaths)
       await send_message(message, message_to_send)
@@ -59,9 +59,9 @@ async def command(client, message):
         message_to_send = "Something went wrong pulling data. Please try again later."
         await send_message(message, message_to_send, True)
       country = data['country']
-      cases = '{:,}'.format(data['cases'])
-      active = '{:,}'.format(data['active'])
-      deaths = '{:,}'.format(data['deaths'])
+      cases = '{:,}'.format(data['cases'] if data['cases'] else 0)
+      active = '{:,}'.format(data['active'] if data['active'] else 0)
+      deaths = '{:,}'.format(data['deaths'] if data['deaths'] else 0)
       message_to_send = "Information for %s\nCases: %s\nActives: %s\nDeaths: %s" % (country, cases, active, deaths)
       await send_message(message, message_to_send)
     
